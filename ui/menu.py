ef14bfbc969935f76ced1f029c77eee785a4a237
Anthony Vidal <vidal.anthony.ffc@gmail.com>
2026-01-05 15:18:33 +0100

    Creation menu utlisateur

--- ui/menu.py
+++ ui/menu.py
@@ -1,1 +1,26 @@
+# Fonction menu principal
+def menu_principal():
+    print("================== Menu principal ====================")
+    print("====== Quelle action voulez-vous effectuer ? =======")
+    print(" Choix 1 : Chiffrer un fichier ou un message.")
+    print(" Choix 2 : Déchiffrer un fichier ou un message.")
+    print(" Choix 3 : Quitter.")
+    choix = input("Votre choix : ")
 
+# Fonction menu chiffrement
+def menu_chiffrement():
+    print("================== Menu de chiffrement ====================")
+    print("====== Quelle action voulez-vous effectuer ? =======")
+    print(" Choix 1 : Chiffrer un fichier.")
+    print(" Choix 2 : Chiffrer un message.")
+    print(" Choix 3 : Retourner au menu principal.")
+    choix_chiffrement = input("Votre choix : ")
+
+# Fonction menu dechiffrement
+def menu_dechiffrement():
+    print("================== Menu de déchiffrement ====================")
+    print("====== Quelle action voulez-vous effectuer ? =======")
+    print(" Choix 1 : Déchiffrer un fichier.")
+    print(" Choix 2 : Déchiffrer un message.")
+    print(" Choix 3 : Retourner au menu principal.")
+    choix_dechiffrement = input("Votre choix : ")
